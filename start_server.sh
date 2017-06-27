d28aa4967587f983e685c5fcd5607ec4c532c929
manpat <pat@patsown.com>
2017-06-27 04:00:37 +0100

    Update start_server

--- start_server.sh
+++ start_server.sh
@@ -6,6 +6,5 @@ for session in $(screen -ls | grep -o '[0-9]*\.server'); do
 	screen -S "${session}" -X quit;
 done
 
-screen -dmS "server"
-sleep 0.5 # this is the best I could do, whatever man
-screen -r -S "server" -X screen bash -ic "RUST_BACKTRACE=1 cargo run"
+screen -dmS "server" bash -i
+screen -r -S "server" -p 0 -X stuff $'RUST_BACKTRACE=1 cargo run\r'
